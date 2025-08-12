eceeddddf0dc05e4a2886a75a81c50b2ae35563f
Mags17-dot <magdapretorius143@gmail.com>
2025-08-12 18:49:40 +0200

    Added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Hello World");
+function generatePoem(event) {
+    event.preventDefault();
+
+new Typewriter("#poem", {
+    strings: 
+        "I was not happy with the rain until my eyes caught five wet leaves"
+    ,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+});
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
