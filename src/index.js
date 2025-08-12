a371d3b89a5da003c11ead90201eba918c9152ac
Mags17-dot <magdapretorius143@gmail.com>
2025-08-12 20:27:53 +0200

    Added API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-function generatePoem(event) {
-    event.preventDefault();
-
-new Typewriter("#poem", {
-    strings: 
-        "I was not happy with the rain until my eyes caught five wet leaves"
-    ,
+function displayPoem(response) {
+    console.log("poem generated");
+    new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
 });
 }
+
+
+
+
+
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+let apiKey = "083468eba6ce58a07346b8t5d1ac0fod";
+let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+let context = "You are a short Poem expert. Your passion is to generate a poem in basic HTML. Make sure to follow the user instructions.";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log("Generating poem");
+console.log(`Prompt: ${prompt}`);
+console.log(`Context: ${context}`);
+
+
+axios.get(apiUrl).then(displayPoem);
+}
+
+
+
+
+
+
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
