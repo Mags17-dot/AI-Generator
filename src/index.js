8d14b7f9474e5c37b57c61b6d4e09b0b7a8e62e6
Mags17-dot <magdapretorius143@gmail.com>
2025-08-14 18:31:49 +0200

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-    console.log("poem generated");
     new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -7,10 +6,6 @@ function displayPoem(response) {
     cursor: "",
 });
 }
-
-
-
-
 
 
 function generatePoem(event) {
@@ -22,9 +17,10 @@ let prompt = `User instructions: Generate a poem about ${instructionsInput.value
 let context = "You are a short Poem expert. Your passion is to generate a poem in basic HTML. Make sure to follow the user instructions.";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-console.log("Generating poem");
-console.log(`Prompt: ${prompt}`);
-console.log(`Context: ${context}`);
+
+let poemElement =  document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating">⏳Generating poem about ${instructionsInput.value}...</div>`;
 
 
 axios.get(apiUrl).then(displayPoem);
